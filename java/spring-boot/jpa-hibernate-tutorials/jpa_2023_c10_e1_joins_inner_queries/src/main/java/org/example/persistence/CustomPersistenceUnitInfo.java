99bf7de15c69ffcffb86fc968651a19e77a71c0e
fadewraith <doodledante22@gmail.com>
2024-10-26 15:57:08 +0530

    added code for jpql queries joins and inner joins jpa/hibernate

---
+++ java/spring-boot/jpa-hibernate-tutorials/jpa_2023_c10_e1_joins_inner_queries/src/main/java/org/example/persistence/CustomPersistenceUnitInfo.java
@@ -0,0 +1,120 @@
+package org.example.persistence;
+
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.SharedCacheMode;
+import jakarta.persistence.ValidationMode;
+import jakarta.persistence.spi.ClassTransformer;
+import jakarta.persistence.spi.PersistenceUnitInfo;
+import jakarta.persistence.spi.PersistenceUnitTransactionType;
+
+import javax.sql.DataSource;
+import java.net.URL;
+import java.util.List;
+import java.util.Properties;
+
+public class CustomPersistenceUnitInfo implements PersistenceUnitInfo {
+
+//    other way to get an entity manager factory, one is in the main class, i.e. xml file
+
+    private final String persistenceUnitName;
+
+    public CustomPersistenceUnitInfo(String persistenceUnitName) {
+        this.persistenceUnitName = persistenceUnitName;
+    }
+
+    @Override
+    public String getPersistenceUnitName() {
+//        this name is unique for each persistence
+        return persistenceUnitName;
+    }
+
+    @Override
+    public String getPersistenceProviderClassName() {
+        return "org.hibernate.jpa.HibernatePersistenceProvider";
+    }
+
+    @Override
+    public PersistenceUnitTransactionType getTransactionType() {
+        return PersistenceUnitTransactionType.RESOURCE_LOCAL; // JTA or RESOURCE_LOCAL
+    }
+
+    @Override
+    public DataSource getJtaDataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl("jdbc:mysql://localhost/");
+        dataSource.setUsername("");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    @Override
+    public List<String> getManagedClassNames() {
+//         to allow app manipulate entities for reflection
+//        specify all entities here, otherwise it might not work
+        return List.of(
+                "org.example.entities.Course",
+                "org.example.entities.Student",
+                "org.example.entities.Enrollment"
+        );
+    }
+
+    @Override
+    public DataSource getNonJtaDataSource() {
+        return null;
+    }
+
+    @Override
+    public List<String> getMappingFileNames() {
+        return null;
+    }
+
+    @Override
+    public List<URL> getJarFileUrls() {
+        return null;
+    }
+
+    @Override
+    public URL getPersistenceUnitRootUrl() {
+        return null;
+    }
+
+    @Override
+    public boolean excludeUnlistedClasses() {
+        return false;
+    }
+
+    @Override
+    public SharedCacheMode getSharedCacheMode() {
+        return null;
+    }
+
+    @Override
+    public ValidationMode getValidationMode() {
+        return null;
+    }
+
+    @Override
+    public Properties getProperties() {
+        return null;
+    }
+
+    @Override
+    public String getPersistenceXMLSchemaVersion() {
+        return null;
+    }
+
+    @Override
+    public ClassLoader getClassLoader() {
+        return null;
+    }
+
+    @Override
+    public void addTransformer(ClassTransformer transformer) {
+
+    }
+
+    @Override
+    public ClassLoader getNewTempClassLoader() {
+        return null;
+    }
+}

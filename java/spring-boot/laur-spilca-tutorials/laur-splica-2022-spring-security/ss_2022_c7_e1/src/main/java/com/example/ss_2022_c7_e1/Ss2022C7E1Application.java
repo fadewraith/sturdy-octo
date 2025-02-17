354f9a658ead0190f26b6f728afd9f006783b908
fadewraith <doodledante22@gmail.com>
2025-02-17 21:04:53 +0530

    added method authorization 1

---
+++ java/spring-boot/laur-spilca-tutorials/laur-splica-2022-spring-security/ss_2022_c7_e1/src/main/java/com/example/ss_2022_c7_e1/Ss2022C7E1Application.java
@@ -0,0 +1,14 @@
+package com.example.ss_2022_c7_e1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss2022C7E1Application {
+// Method authorization - Part 1
+
+  public static void main(String[] args) {
+    SpringApplication.run(Ss2022C7E1Application.class, args);
+  }
+
+}
---
+++ java/spring-boot/laur-spilca-tutorials/laur-splica-2022-spring-security/ss_2022_c7_e1/src/main/java/com/example/ss_2022_c7_e1/security/Demo4ConditionEvaluator.java
@@ -0,0 +1,4 @@
+package com.example.ss_2022_c7_e1.security;
+
+public class Demo4ConditionEvaluator {
+}

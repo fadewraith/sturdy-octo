9fd4ef4c495007134359b67145abf97f7de94074
fadewraith <doodledante22@gmail.com>
2025-02-22 22:05:25 +0530

    added oauth fundamentals

---
+++ java/spring-boot/laur-spilca-tutorials/laur-splica-2022-spring-security/ss_2022_c10_e1/src/main/java/com/example/ss_2022_c10_e1/Ss2022C10E1Application.java
@@ -0,0 +1,14 @@
+package com.example.ss_2022_c10_e1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss2022C10E1Application {
+// Method authorization - Part 1
+
+  public static void main(String[] args) {
+    SpringApplication.run(Ss2022C10E1Application.class, args);
+  }
+
+}
---
+++ java/spring-boot/laur-spilca-tutorials/laur-splica-2022-spring-security/ss_2022_c10_e1/src/main/resources/README.md
@@ -0,0 +1,8 @@
+1. SecurityConfig
+2. ApiKeyFilter
+3. ApiKeyAuthenticaton
+4. CustomAuthenticationManager
+5. ApiKeyProvider
+
+Method Level Authorization (ASPECT) -
+Controller -> Service

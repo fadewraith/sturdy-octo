b4c39886acf23fd07102e1c45e767b8d3d220af1
fadewraith <doodledante22@gmail.com>
2025-04-03 23:40:53 +0530

    removed incomplete java dsa codes

--- dsa/dsa-using-java/src/utils/DoublyNode.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-public class DoublyNode {
-    public int value;
-    public DoublyNode next;
-    public DoublyNode prev;
-}
--- dsa/dsa-using-java/src/utils/Node.java
+++
@@ -1,6 +0,0 @@
-package utils;
-
-public class Node {
-    public int value;
-    public Node next;
-}
